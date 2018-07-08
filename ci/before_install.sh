2a8c3846c5b76f792388aa621130953160d994a5
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-08 17:28:29 +0200

    Update travis CI

---
+++ ci/before_install.sh
@@ -0,0 +1,31 @@
+set -e
+
+function llvm_version_triple() {
+    if [ "$1" == "4.0" ]; then
+        echo "4.0.0"
+    elif [ "$1" == "5.0" ]; then
+        echo "5.0.0"
+    fi
+}
+
+function llvm_download() {
+    export LLVM_VERSION_TRIPLE=`llvm_version_triple ${LLVM_VERSION}`
+    export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-x86_64-$1
+
+    wget http://llvm.org/releases/${LLVM_VERSION_TRIPLE}/${LLVM}.tar.xz
+    mkdir llvm
+    tar xf ${LLVM}.tar.xz -C llvm --strip-components=1
+
+    export LLVM_CONFIG_PATH=`pwd`/llvm/bin/llvm-config
+}
+
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    llvm_download linux-gnu-ubuntu-14.04
+    export LD_LIBRARY_PATH=`pwd`/llvm/lib:$LD_LIBRARY_PATH
+else
+    llvm_download apple-darwin
+    export DYLD_LIBRARY_PATH=`pwd`/llvm/lib:$DYLD_LIBRARY_PATH
+fi
+
+export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
+export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,11 @@ set -e
 
 export CARGO_TARGET_DIR=`pwd`/target
 
-RUST_BACKTRACE=1 cargo test --verbose -- --nocapture
+if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    CLANG_VERSION="$CLANG_VERSION clippy"
+fi
+
+RUST_BACKTRACE=1 cargo test --verbose --features $CLANG_VERSION -- --nocapture
+RUST_BACKTRACE=1 cargo test --verbose --features "$CLANG_VERSION runtime" -- --nocapture
 
 cargo doc --no-deps
