6abc869ec5579815e1c17a779122983fe1f10813
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-08 18:08:52 +0200

    Update travis CI

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -4,25 +4,9 @@ if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     rvm get head || true
 fi
 
-function llvm_linux_target_triple() {
-    if [ "$1" == "5.0" ]; then
-        echo "linux-x86_64-ubuntu14.04"
-    else
-        echo "x86_64-linux-gnu-ubuntu-14.04"
-    fi
-}
-
-function llvm_version_triple() {
-    if [ "$1" == "4.0" ]; then
-        echo "4.0.0"
-    elif [ "$1" == "5.0" ]; then
-        echo "5.0.0"
-    fi
-}
-
 function llvm_download() {
-    export LLVM_VERSION_TRIPLE=`llvm_version_triple ${LLVM_VERSION}`
-    export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-$1
+    export LLVM_VERSION_TRIPLE="6.0.0"
+    export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-x86_64-$1
     export LLVM_DIRECTORY="$HOME/.llvm/${LLVM}"
 
     if [ -d "${LLVM_DIRECTORY}" ]; then
@@ -37,12 +21,10 @@ function llvm_download() {
 }
 
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-    llvm_download `llvm_linux_target_triple ${LLVM_VERSION}`
+    llvm_download linux-gnu-ubuntu-14.04
     export LD_LIBRARY_PATH="${LLVM_DIRECTORY}/lib":$LD_LIBRARY_PATH
 else
     llvm_download x86_64-apple-darwin
     export DYLD_LIBRARY_PATH="${LLVM_DIRECTORY}/lib":$DYLD_LIBRARY_PATH
 fi
 
-popd
-set +e
