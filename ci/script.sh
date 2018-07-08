ea666246f4d94272f55ef989f8f3a97a1872858b
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-08 16:36:40 +0200

    Update travis CI

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,6 @@ set -e
 
 export CARGO_TARGET_DIR=`pwd`/target
 
-RUST_BACKTRACE=1 cargo test --verbose -- --nocapture
+RUST_BACKTRACE=1 cargo test --verbose --features clang_6_0 -- --nocapture
 
 cargo doc --no-deps
