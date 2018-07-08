3b13bbe3d20092052381b7b28714555bfe2178d6
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-08 15:03:02 +0200

    Update travis CI and tests

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,6 @@ set -e
 
 export CARGO_TARGET_DIR=`pwd`/target
 
-RUST_BACKTRACE=1 cargo test --verbose --features $CLANG_VERSION -- --nocapture
-RUST_BACKTRACE=1 cargo test --verbose --features "$CLANG_VERSION runtime" -- --nocapture
+RUST_BACKTRACE=1 cargo test --verbose -- --nocapture
 
 cargo doc --no-deps
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,4 @@
-extern crate log;
 extern crate thinlinelib;
-
-use log::LogLevel;
-use thinlinelib::Thinline;
-use thinlinelib::analysis_c::C_FILE_EXTENSIONS;
 
 #[test]
 fn test_new() {}
