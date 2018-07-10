52bc67eb09c8ac8c4a0f08b48048a45ca50a7d11
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-10 16:37:59 +0200

    Simplify CI

--- ci/before_script.sh
+++
@@ -1,8 +0,0 @@
-pip install 'travis-cargo<0.2' --user --verbose 
-
-export PATH=$HOME/.local/bin:$PATH
-export PATH=$HOME/Library/Python/2.7/bin:$PATH
-
-if [ $(cargo install --list | grep cargo-kcov) ]; then
-    cargo install cargo-kcov
-fi
