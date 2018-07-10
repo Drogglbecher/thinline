73bab4b0669e8725dc8f0b76ba3cd7800dcc8056
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-10 15:45:08 +0200

    Respect cache for kcov installation

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -3,4 +3,6 @@ pip install 'travis-cargo<0.2' --user --verbose
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/Library/Python/2.7/bin:$PATH
 
-cargo install cargo-kcov
+if [ $(cargo install --list | grep cargo-kcov) ]; then
+    cargo install cargo-kcov
+fi
