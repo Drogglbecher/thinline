193f245a62aa221bdfe7d295157ee24d799f1a30
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-10 15:29:16 +0200

    Add clang 5.0 build

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,5 +1,8 @@
-if [[ "$TRAVIS_RUST_VERSION" == "stable" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    travis-cargo doc-upload;
-    cargo kcov --print-install-kcov-sh | sh;
-    cargo kcov --coveralls --kcov ./kcov-33/build/src/kcov;
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    if [[ "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+        travis-cargo doc-upload;
+    elif [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+        cargo kcov --print-install-kcov-sh | sh;
+        cargo kcov --coveralls --kcov ./kcov-33/build/src/kcov;
+    fi
 fi
--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,18 +1,26 @@
 set -e
+pushd ~
 
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     rvm get head || true
 fi
 
+function llvm_linux_target() {
+    if [ "$1" == "5.0" ]; then
+        echo "linux-x86_64-ubuntu14.04"
+    else
+        echo "x86_64-linux-gnu-ubuntu-14.04"
+    fi
+}
+
 function llvm_download() {
-    export LLVM_VERSION_TRIPLE="6.0.0"
-    export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-x86_64-$1
+    export LLVM=clang+llvm-${LLVM_VERSION}.0-$1
     export LLVM_DIRECTORY="$HOME/.llvm/${LLVM}"
 
     if [ -d "${LLVM_DIRECTORY}" ]; then
         echo "Using cached LLVM download for ${LLVM}..."
     else
-        wget http://llvm.org/releases/${LLVM_VERSION_TRIPLE}/${LLVM}.tar.xz --debug --verbose
+        wget http://llvm.org/releases/${LLVM_VERSION}.0/${LLVM}.tar.xz --debug --verbose
         mkdir -p "${LLVM_DIRECTORY}"
         tar xf ${LLVM}.tar.xz -C "${LLVM_DIRECTORY}" --strip-components=1
     fi
@@ -21,10 +29,13 @@ function llvm_download() {
 }
 
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-    llvm_download linux-gnu-ubuntu-14.04
+    llvm_download `llvm_linux_target ${LLVM_VERSION}`
     export LD_LIBRARY_PATH="${LLVM_DIRECTORY}/lib":$LD_LIBRARY_PATH
 else
     llvm_download x86_64-apple-darwin
+    cp "${LLVM_DIRECTORY}/lib/libclang.dylib" /usr/local/lib/libclang.dylib
     export DYLD_LIBRARY_PATH="${LLVM_DIRECTORY}/lib":$DYLD_LIBRARY_PATH
 fi
 
+popd
+set +e
