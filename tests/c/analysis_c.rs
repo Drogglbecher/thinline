07540c5ef82944855da7fd34f1e65889e4bbb13b
Sebastian Spiess <spiess.sebastian@web.de>
2018-07-12 20:03:42 +0200

    Extend analysis_c tests

--- tests/c/analysis_c.rs
+++ tests/c/analysis_c.rs
@@ -1,6 +1,5 @@
 extern crate thinlinelib;
 
-use std::path::Path;
 use thinlinelib::analysis::Analysis;
 use thinlinelib::c::analysis_c::{AnalysisC, C_FILE_EXTENSIONS};
 
@@ -12,12 +11,58 @@ fn test_new() {
     assert_eq!(analysis.project_files().len(), 0);
 }
 
-#[test]
-fn test_collect_sources() {
-    let c_test_src_path = Path::new("tests").join("testdata").join("c_sources");
-    let analysis = AnalysisC::new();
+#[cfg(test)]
+mod test_collect_sources {
 
-    assert!(analysis.collect_sources(&c_test_src_path, &["."]).is_ok());
+    #[cfg(test)]
+    mod should_succeed {
 
-    assert_eq!(analysis.project_files().len(), 3);
+        use std::path::Path;
+        use thinlinelib::analysis::Analysis;
+        use thinlinelib::c::analysis_c::AnalysisC;
+
+        // Should succeed
+        #[test]
+        fn when_directory_is_valid() {
+            // Given
+            let analysis = AnalysisC::new();
+
+            // When
+            let c_test_src_path = Path::new("tests").join("testdata").join("c_sources");
+            assert!(analysis.collect_sources(&c_test_src_path, &["."]).is_ok());
+
+            // Then
+            assert_eq!(analysis.project_files().len(), 5);
+        }
+    }
+
+    #[cfg(test)]
+    mod should_fail {
+
+        use std::path::Path;
+        use thinlinelib::analysis::Analysis;
+        use thinlinelib::c::analysis_c::AnalysisC;
+
+        // Directory not existing
+        #[test]
+        fn when_directory_not_existing() {
+            // Given
+            let analysis = AnalysisC::new();
+            let c_test_src_path = Path::new("not").join("existing");
+
+            // Then
+            assert!(analysis.collect_sources(&c_test_src_path, &["."]).is_err());
+        }
+
+        // Path is no directory
+        #[test]
+        fn when_path_is_no_directory() {
+            // Given
+            let analysis = AnalysisC::new();
+            let c_test_src_path = Path::new("tests").join("lib.rs");
+
+            // Then
+            assert!(analysis.collect_sources(&c_test_src_path, &["."]).is_err());
+        }
+    }
 }
