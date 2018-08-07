f4271fcfc7a6b39590baa8721933cd83918429a5
Sebastian Spiess <spiess.sebastian@web.de>
2018-08-07 20:50:37 +0200

    Add testfile struct

--- src/synthesis.rs
+++ src/synthesis.rs
@@ -1,2 +1,16 @@
+use language_type::LanguageType;
+use std::marker::PhantomData;
+
 #[derive(Default, Debug)]
 pub struct Synthesis {}
+
+#[derive(Default, Debug)]
+pub struct TestFile<T> {
+    pub pf_type: PhantomData<T>,
+}
+
+/// Reprensents a test file.
+impl<T> TestFile<T>
+where
+    T: LanguageType,
+{}
