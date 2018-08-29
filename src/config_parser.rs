69bb8f526202298580a8c2db0a402b3c03c3acf3
Sebastian Spiess <spiess.sebastian@web.de>
2018-08-29 21:08:51 +0200

    Extend tests for config parser

--- src/config_parser.rs
+++ src/config_parser.rs
@@ -133,7 +133,7 @@ mod value_parser {
     use std::fs::read_to_string;
     use std::path::Path;
     use super::ValueParser;
-    use yaml_rust::{Yaml, YamlLoader};
+    use yaml_rust::YamlLoader;
 
     #[test]
     fn parse_yaml_config_str_succeed() {
@@ -151,19 +151,24 @@ mod value_parser {
         let yml_params = YamlLoader::load_from_str(read_to_string(yml_path).unwrap().as_str()).unwrap();
         let yml_param = yml_params.get(0);
 
-        let test_env = yml_param.unwrap().get_str(&["none_existing"]);
-        assert!(test_env.is_none());
-    }
+        {
+            let test_env = yml_param.unwrap().get_str(&["none_existing"]);
+            assert!(test_env.is_none());
+        }
 
-    #[test]
-    fn parse_yaml_config_str_vec() {
+        {
+            let test_env = yml_param.unwrap().get_str(&[]);
+            assert!(test_env.is_none());
+        }
 
-        let yml_path = Path::new("tests").join("testdata").join("config").join("config1.yml");
+        let yml_path = Path::new("tests").join("testdata").join("config").join("config4.yml");
         let yml_params = YamlLoader::load_from_str(read_to_string(yml_path).unwrap().as_str()).unwrap();
         let yml_param = yml_params.get(0);
 
-        let test_env = yml_param.unwrap().get_str_vec(&["include_dirs"]);
-        assert_eq!(test_env, Some(vec!["include", "src"]));
+        {
+            let test_env = yml_param.unwrap().get_str(&["test_env"]);
+            assert!(test_env.is_none());
+        }
     }
 
     #[test]
@@ -172,7 +177,19 @@ mod value_parser {
         let yml_params = YamlLoader::load_from_str(read_to_string(yml_path).unwrap().as_str()).unwrap();
         let yml_param = yml_params.get(0);
 
-        let test_env = yml_param.unwrap().get_str_vec(&["none_existing"]);
-        assert!(test_env.is_none());
+        {
+            let test_env = yml_param.unwrap().get_str_vec(&["include_dirs"]);
+            assert_eq!(test_env, Some(vec!["include", "src"]));
+        }
+
+        {
+            let test_env = yml_param.unwrap().get_str_vec(&["none_existing"]);
+            assert!(test_env.is_none());
+        }
+
+        {
+            let test_env = yml_param.unwrap().get_str_vec(&[]);
+            assert!(test_env.is_none());
+        }
     }
 }
