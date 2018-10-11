8e6c00ca166e1a53dade8c9a09bbdef2e4c298a9
Sebastian Spiess <code@sebastian-spiess.de>
2018-10-11 19:54:16 +0200

    Add build script runner

--- src/config_parser.rs
+++ src/config_parser.rs
@@ -1,5 +1,6 @@
 use failure::{err_msg, Fallible};
-use std::fs::read_to_string;
+use run_script::ScriptOptions;
+use std::{env, fs::read_to_string};
 use yaml_rust::{Yaml, YamlLoader};
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -22,6 +23,9 @@ impl<'a> Conversion for Option<Vec<&'a str>> {
 ////////////////////////////////////////////////////////////////////////////////
 
 trait ValueParser {
+    /// Reads a bool at the given config path.
+    fn get_bool(&self, keys: &[&str], default: bool) -> bool;
+
     /// Reads a string at the given config path.
     fn get_str(&self, keys: &[&str]) -> Option<&str>;
 
@@ -30,6 +34,27 @@ trait ValueParser {
 }
 
 impl ValueParser for Yaml {
+    fn get_bool(&self, keys: &[&str], default: bool) -> bool {
+        let mut yml_obj = self;
+
+        for key in keys {
+            if yml_obj[*key].is_badvalue() {
+                return default;
+            }
+
+            yml_obj = &yml_obj[*key];
+            if let Some(yml_bool) = yml_obj.as_bool() {
+                return yml_bool;
+            }
+
+            if yml_obj[0].is_badvalue() {
+                continue;
+            }
+        }
+
+        default
+    }
+
     fn get_str(&self, keys: &[&str]) -> Option<&str> {
         let mut yml_obj = self;
 
@@ -86,8 +111,70 @@ impl ValueParser for Yaml {
 
 #[derive(Default, Debug)]
 pub struct BuildScript {
+    /// Indicator whether the output is logged within thinline output or not.
+    pub log: bool,
+
+    /// Windows build steps.
     pub windows: Vec<String>,
+
+    /// Linux build steps.
     pub linux: Vec<String>,
+}
+
+impl BuildScript {
+    /// Executes the build script depending on the target OS.
+    ///
+    /// On Windows the script is executed with `cmd`, on linux with `sh`.
+    /// All build steps are concatenated with ` && `.
+    /// Given build steps are executed at the target project directory,
+    /// not at thinline directory. The yaml param `log` at `build_script`
+    /// section indicates whether the build outpus is print within the
+    /// thinline output (true) or the child process (false).
+    pub fn run(&self, dir: &str) -> Fallible<()> {
+        info!("Building target");
+
+        // Save current working dir
+        let current_working_dir = env::current_dir()?;
+
+        // Change to project dir
+        env::set_current_dir(&dir)?;
+
+        // Build script options
+        let mut options = ScriptOptions::new();
+
+        // Set the runner.
+        options.runner = if cfg!(target_os = "windows") {
+            Some(String::from("cmd"))
+        } else {
+            Some(String::from("sh"))
+        };
+
+        // Print it to the parent process output.
+        options.capture_output = !self.log;
+
+        // Format the commands depending on OS.
+        let cmd = if cfg!(target_os = "windows") {
+            format!(r#"{}"#, self.windows.join(" && "))
+        } else {
+            format!(r#"{}"#, self.linux.join(" && "))
+        };
+
+        // Run the script
+        let (code, _, _) = run_script::run(cmd.as_str(), &vec![], &options)?;
+
+        // Change back to thinline dir
+        env::set_current_dir(&current_working_dir)?;
+
+        // Check the return code
+        if code > 0 {
+            return Err(err_msg(format!(
+                "Executing build steps returned error code {}.",
+                code
+            )));
+        }
+
+        Ok(())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -132,6 +219,7 @@ impl ProjectParameters {
 
                 params.source_dirs = yml_param.get_str_vec(&["analysis_dirs"]).to_string_vec();
                 params.include_dirs = yml_param.get_str_vec(&["include_dirs"]).to_string_vec();
+                params.build_script.log = yml_param.get_bool(&["build_script", "log"], true);
                 params.build_script.linux = yml_param
                     .get_str_vec(&["build_script", "linux"])
                     .to_string_vec();
@@ -156,6 +244,36 @@ mod value_parser {
     use std::fs::read_to_string;
     use std::path::Path;
     use yaml_rust::YamlLoader;
+
+    #[test]
+    fn parse_yaml_config_bool_succeed() {
+        let yml_path = Path::new("tests")
+            .join("testdata")
+            .join("config")
+            .join("config1.yml");
+        let yml_params =
+            YamlLoader::load_from_str(read_to_string(yml_path).unwrap().as_str()).unwrap();
+        let yml_param = yml_params.get(0);
+
+        let build_log = yml_param.unwrap().get_bool(&["build_script", "log"], false);
+        assert_eq!(build_log, true);
+    }
+
+    #[test]
+    fn parse_yaml_config_bool_failed() {
+        let yml_path = Path::new("tests")
+            .join("testdata")
+            .join("config")
+            .join("config1.yml");
+        let yml_params =
+            YamlLoader::load_from_str(read_to_string(yml_path).unwrap().as_str()).unwrap();
+        let yml_param = yml_params.get(0);
+
+        let build_log = yml_param
+            .unwrap()
+            .get_bool(&["build_script", "none_existing"], true);
+        assert_eq!(build_log, true);
+    }
 
     #[test]
     fn parse_yaml_config_str_succeed() {
